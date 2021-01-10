de7c599454a5eeddefaa7211a02c47e12ef0d20c
Ingo Kleiber <ikleiber@gmail.com>
2021-01-10 17:21:07 +0100

    Optimize downloaders

--- 2020/data/download_coca.sh
+++ 2020/data/download_coca.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 mkdir -p corpora/coca
+mkdir temp
 
+cd temp
 wget https://www.corpusdata.org/coca/samples/coca-samples-text.zip
-unzip coca-samples-text.zip -d ./corpora/coca
+unzip coca-samples-text.zip -d ./../corpora/coca
+
+cd ..
+rm -r temp
--- 2020/data/download_hum19uk.sh
+++ 2020/data/download_hum19uk.sh
@@ -10,4 +10,7 @@ wget https://www.linguisticsathuddersfield.com/s/1850-1899.zip
 unzip '*.zip' -d ./hum19uk
 
 cd hum19uk
-unzip '*.zip' -d ../../corpora/hum19uk
+unzip '*.zip' -d ./../../corpora/hum19uk
+
+cd ..
+rm -r temp
